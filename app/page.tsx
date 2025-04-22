c2ba5c378509ddc5ed80cfb7feb884e258815955
boggledstarsector <boggledstarsector@gmail.com>
2025-04-22 16:56:44 -0400

    added chat pane

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
+import ChatPane from "@/components/ChatPane";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
       <h1 className={ styles.cjremmett_header}><a className = { styles.cjremmett_link } href="https://cjremmett.com">cjremmett.com</a></h1>
+      <ChatPane />
     </>
   );
 }
---
+++ components/ChatPane.tsx
@@ -0,0 +1,10 @@
+
+import styles from './ChatPane.module.css';
+
+export default function ChatPane() {
+  return (
+    <>
+      <div className={ styles.test }>Hello world!</div>
+    </>
+  );
+}
