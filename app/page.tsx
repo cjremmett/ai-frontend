f656ba7f6baf3717d515518e2f8c7eb39186f4e7
boggledstarsector <boggledstarsector@gmail.com>
2025-04-22 16:47:16 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className={ styles.cjremmett_header}><a className = { styles.cjremmett_link } href="https://cjremmett.com">cjremmett.com</a></h1>
+    </>
+  );
+}
