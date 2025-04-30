4f7fc372f58bf19257982e809ef527ac73a0e54d
Christopher Remmett <boggledstarsector@gmail.com>
2025-04-30 16:39:18 -0400

    some work to implement websockets

---
+++ app/page.js
@@ -0,0 +1,45 @@
+
+import React, { useState, useEffect } from 'react';
+import ChatWindow from "@/components/ChatWindow/ChatWindow";
+import styles from "./page.module.css";
+import { socket } from '../socket';
+import { ConnectionState } from './components/ConnectionState';
+import { ConnectionManager } from './components/ConnectionManager';
+import { Events } from "./components/Events";
+import { MyForm } from './components/MyForm';
+
+export default function Home() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents(previous => [...previous, value]);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1 className={ styles.cjremmett_header}><a className = { styles.cjremmett_link } href="https://cjremmett.com">cjremmett.com</a></h1>
+      <ChatWindow />
+    </>
+  );
+}
